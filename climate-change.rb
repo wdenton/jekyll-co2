741e05f78932666831529d1666cd5e8bf7e07d16
William Denton <wtd@pobox.com>
2014-06-07 22:52:02 -0400

    Hacking

--- climate-change.rb
+++ climate-change.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
+require 'date'
 require 'open-uri'
+require 'json'
 
 mlo_data = "ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt"
 
@@ -27,4 +29,24 @@ rescue Exception => e
   STDERR.puts "Could not download data: #{e}"
 end
 
-puts data
+# Now we want to get the most recent month of data available
+# If it isn't the current month, step back 28 days.
+# That should always be enough.
+
+yyyymm = Date.today.strftime("%Y-%m")
+
+if data.has_key? yyyymm
+  previous_year = (Date.today - 365).strftime("%Y-%m")
+else
+  yyyymm = (Date.today - 28).strftime("%Y-%m")
+  previous_year = (Date.today - 28 - 365 ).strftime("%Y-%m")
+end
+
+puts yyyymm
+puts data[yyyymm]
+puts previous_year
+puts data[previous_year]
+
+annual_increase = 100 * data[yyyymm]["interpolated"].to_f / data[previous_year]["interpolated"].to_f
+
+puts annual_increase
