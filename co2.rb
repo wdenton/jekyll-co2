b0a44dad6e06217e3354e92b793c2a5a33979b01
William Denton <wtd@pobox.com>
2020-07-02 14:32:59 -0400

    Display only text and numbers, no sparkline

--- co2.rb
+++ co2.rb
@@ -16,8 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Jekyll CO₂.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright 2014, 2017 William Denton <wtd@pobox.com>
+# Copyright 2014--2020 William Denton <wtd@pobox.com>
 
+require "csv"
 require "date"
 require "open-uri"
 
@@ -35,47 +36,48 @@ module Jekyll
       Date::MONTHNAMES[mmm.to_i]
     end
 
-    TICKS = %w[▁ ▂ ▃ ▄ ▅ ▆ ▇].freeze
-
     def generate(site)
-      # We get the data by downloading and parsing a text file, and we
-      # only want to do this once.  Here we get the file and do some
-      # work and build the chunk of HTML that will be added to any
-      # web page with the {% co2 %} tag on it.  The actual Liquid tag,
-      # below, really doesn't do anything except output the HTML.
-
+      #
       # See http://www.esrl.noaa.gov/gmd/ccgg/trends/ for additional details.
-      mlo_data = "ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt"
+      mlo_csv_url = "ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.csv"
+
+      site_data_dir = site.source + "/_data/"
+      mlo_csv = site_data_dir + "co2_mm_mlo.csv"
+
+      years_to_sample = 50
+
+      unless Dir.exist?(site_data_dir)
+        warn "Creating #{site_data_dir}"
+        Dir.mkdir(site_data_dir)
+      end
+
+      # Download the data and store locally.
+      begin
+        File.open(mlo_csv, "wt") do |file|
+          file << URI.open(mlo_csv_url).read.gsub(/^#.*\n/, "") # Strip all the comments.
+        end
+      rescue Net::FTPTempError => e
+        Jekyll.logger warn "Could not download data: #{e}; using stored file"
+        exit
+      end
 
       # Store all the data here so we can easily pick out the three
       # months we want.
-      data = Hash.new { |h, k| h[k] = {} }
+      co2_data = Hash.new { |h, k| h[k] = {} }
+
+      CSV.foreach(mlo_csv, headers: true, header_converters: :symbol) do |row|
+        yyyy = row[:year]
+        month_num = format("%<name>02d", name: row[:month]) # Add a leading zero if none exists
+        co2_data[yyyy][month_num] = {
+          # "decimal_date" => row[:decimal_date],
+          # "average" => row[:average],
+          "interpolated" => row[:interpolated]
+          # "trend" => row[:trend],
+          # "days" => row[:days]
+        }
+      end
 
       co2_html = ""
-
-      begin
-        URI.open(mlo_data) do |f|
-          raw = f.read
-          raw.each_line do |line|
-            next if /^#/.match(line)
-
-            (yyyy, month, decimal_date, average, interpolated, trend, days) = line.split("\s")
-            yyyy = yyyy.to_i
-            mm = format("%<name>02d", name: month) # Add a leading zero if none exists
-
-            data[yyyy][mm] = {
-              "decimal_date" => decimal_date,
-              "average" => average,
-              "interpolated" => interpolated,
-              "trend" => trend,
-              "days" => days
-            }
-          end
-        end
-      rescue Net::FTPTempError => e
-        Jekyll.logger warn "Could not download data: #{e}"
-        exit
-      end
 
       # Now we want to get the most recent month of data available
       # It will never be the current month, because it's not over yet.
@@ -83,40 +85,47 @@ module Jekyll
       # month before that (for example if it's 01 June, the May data
       # may not be processed yet, so we need to get April's).
 
-      monthname = ""
       latest_year = (DateTime.now << 1).strftime("%Y").to_i # Subtracts one year
       latest_month = (DateTime.now << 1).strftime("%m") # Keep as string, need leading 0
 
-      mm = ""
+      month_num = ""
       yyyy = ""
 
-      if data[latest_year][latest_month]
+      if co2_data[latest_year][latest_month]
         # << 1 subtracts one month.
-        mm = (DateTime.now << 1).strftime("%m")
-        monthname = month_name(mm)
+        month_num = (DateTime.now << 1).strftime("%m")
         yyyy = (DateTime.now << 1).strftime("%Y").to_i
       else
-        mm = (DateTime.now << 2).strftime("%m")
-        monthname = month_name(mm)
+        month_num = (DateTime.now << 2).strftime("%m")
         yyyy = (DateTime.now << 2).strftime("%Y").to_i
       end
 
-      years_to_sample = 20
+      monthname = month_name(month_num)
 
-      # Sparklines taken from https://gist.github.com/jcromartie/1367091
-      values = (yyyy - years_to_sample..yyyy).map { |x| data[x][mm]["interpolated"] }
-      min = values.min.to_f
-      range = values.max.to_f - values.min.to_f
-      scale = TICKS.length - 1
-      sparkline = values.map.with_index do |x, i|
-        %(<span title="#{yyyy - (years_to_sample - i)}: #{x}">#{TICKS[(((x.to_f - min) / range) * scale).round]}</span>)
-      end.join
+      yyyy_then = (yyyy - years_to_sample).to_s
+      co2_then = co2_data[yyyy_then][month_num.to_s]["interpolated"].to_f
+
+      # mm = "05"
+
+      co2_now = co2_data[yyyy.to_s][month_num.to_s]["interpolated"].to_f
+
+      co2_increase = (co2_now - co2_then).round(2)
+      co2_growth = (100 * co2_increase / co2_then).round(1)
 
       co2_html = <<~HTML
         <div id="co2">
-          <h2>CO₂</h2>
-          <span class="sparkline">#{sparkline}</span>
-          <p><span class="co2_title">Atmospheric CO₂ at Mauna Loa (ppm) in #{monthname} over the last #{years_to_sample} years.</span> </p>
+          <h2>Atmospheric CO₂</h2>
+             <p>
+              #{monthname} #{yyyy_then}: #{co2_then} ppm
+              <br />
+              #{monthname} #{yyyy}: #{co2_now} ppm
+            </p>
+            <p>
+              Increase: #{co2_increase} ppm
+              <br />
+              Change: #{co2_growth} %
+            </p>
+
           <span class="co2_source">
           Sources:
           <a href="http://www.esrl.noaa.gov/gmd/ccgg/trends/">data</a>,
@@ -125,15 +134,9 @@ module Jekyll
         </div>
       HTML
 
-      # This approach is taken from the Stack Overflow question
-      #
-      # Avoid repeated calls to an API in Jekyll Ruby plugin
-      # https://stackoverflow.com/questions/15235023/avoid-repeated-calls-to-an-api-in-jekyll-ruby-plugin
-      #
-
       # Dump the little chunk of HTML to the _includes directory,
-      # and then let it be included with {% include co2.html %}
-      # where desired.
+      # where it can be included with {% include co2.html %} as
+      # needed.
 
       co2_includes_file = site.source + "/_includes/" + "co2.html"
 
